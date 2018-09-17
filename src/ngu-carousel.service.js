2efc972fd0c8ee3d2a808582f15a3c10e71896b4
Furieux33 <haloraptor33@gmail.com>
2018-09-17 14:18:10 +0200

    Update ngu-carousel.service.js

--- src/ngu-carousel.service.js
+++ src/ngu-carousel.service.js
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 var NguCarouselService = /** @class */ (function () {
     function NguCarouselService() {
         this.getData = new Subject();
